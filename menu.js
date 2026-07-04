960e403ff156b67ea8bb5039cadc52e47c446601
Toryabah <sulaimontoheeb5@gmail.com>
2026-07-04 16:50:20 +0100

    Initial website upload

---
+++ menu.js
@@ -0,0 +1,22 @@
+// Toggle Menu-Bar
+
+
+// const menuToggle = document.getElementById("menu-toggle");
+// const navbar = document.getElementById("navbar");
+
+m// enuToggle.addEventListener("click", () => {
+ //    navbar.classList.toggle("active");
+// });
+
+
+
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+});
